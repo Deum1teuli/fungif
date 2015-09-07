c20a2e66ebc282de8b57f22c5da2565c14bf9a51
Deumiteuli <9m1try.mal1k0v@gmail.com>
2015-09-08 01:05:13 +0300

    updateUrl

---
+++ fungif.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         fungif
+// @namespace    http://sc2tv.ru
+// @version      0.1
+// @description  sc2tv & funstream chat gifs paste
+// @author       Domitori
+// @require      https://github.com/carlo/jquery-base64/raw/master/jquery.base64.min.js
+// @include      http://sc2tv.ru/*
+// @match        http://sc2tv.ru/*
+// @include      http://funstream.tv/*
+// @match        http://funstream.tv/*
+// @updateURL    https://raw.githubusercontent.com/Deumiteuli/fungif/master/fungif.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Deumiteuli/fungif/master/fungif.user.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @run-at       document-end
+// ==/UserScript==
